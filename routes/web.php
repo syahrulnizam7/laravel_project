8d9248521c65ceb45b0e21648891f93aa5c1df1b
syahrulnizam <syhrulnizm7@gmail.com>
2025-01-24 17:32:51 +0700

    default

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
+Route::get('/',[TodoController::class,'index'])->name('todo');
 Route::get('/todo',[TodoController::class,'index'])->name('todo');
 Route::post('/todo',[TodoController::class,'store'])->name('todo.post');
 Route::put('/todo/{id}',[TodoController::class,'update'])->name('todo.update');
